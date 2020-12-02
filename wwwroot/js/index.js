bf3a67fd53c9537ba593a7f054402221ce80bb72
Taren <taren.collyer@students.plymouth.ac.uk>
2020-12-02 15:51:47 +0000

    Basic pages added, some JS implemented

---
+++ wwwroot/js/index.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    $('#book').click(function () {
+        $('#page-content-wrapper').load('book.html');
+    })
+
+    $('#index').click(function () {
+        $('#page-content-wrapper').load('indexPanel.html');
+        
+    })
+
+    $('#cancelbooking').click(function () {
+        $('#page-content-wrapper').load('cancelbooking.html');
+    })
+ 
+});
+
+$(document).ready(function GetUserName() {
+    document.getElementById("welcomeMsg").innerHTML = "Welcome, user"; // Will get actual username later on.
+});
